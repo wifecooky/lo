79df53ff4963e47aa62c61c649fbf118765a1f30
wen-test <wen@test.com>
2022-07-21 15:13:48 +0900

    test

--- func.go
+++ func.go
@@ -1,6 +1,7 @@
 package lo
 
 // Partial returns new function that, when called, has its first argument set to the provided value.
+// Test
 func Partial[T1, T2, R any](f func(T1, T2) R, arg1 T1) func(T2) R {
 	return func(t2 T2) R {
 		return f(arg1, t2)
